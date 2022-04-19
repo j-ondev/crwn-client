878f5b11bcd31dffad37f6edfd2a3126c9d9fc6b
João Zorzetti <joao.zorzetti@gmail.com>
2022-04-19 19:49:45 -0300

    Added home and navigation components and routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 
 import './fonts.scss';
 import './index.scss';
@@ -10,7 +11,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
